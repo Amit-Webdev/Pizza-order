2baf79d4b26445e73757a55cdd86e4e6c3d699cd
Amit Kulkarni <154905823+Amit-Webdev@users.noreply.github.com>
2024-08-25 19:00:09 +0530

    Add files via upload

---
+++ src/ui/Loader.jsx
@@ -0,0 +1,10 @@
+function Loader() {
+  // inset-0 means that top, left, right and bottom values will be 0
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default Loader;
